140ef4642cb71b360e25dc05552a9cee00436726
playapez <64451937+playapez@users.noreply.github.com>
2021-11-25 13:03:29 -0500

    Delete FishingTour/fronted directory

--- FishingTour/fronted/js/main.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function ($) {
-    var mainMenu = $('#mainMenu');
-    var mobileMenuButton = $('#mobileMenuButton');
-
-    mobileMenuButton.on('click', function (e) {
-        e.preventDefault();
-        mainMenu.slideToggle();
-    });
-});
